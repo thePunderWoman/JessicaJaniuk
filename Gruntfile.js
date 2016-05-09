49df3e8f6208bdda1ba743ab5b8dd6cb35678868
Jessica Janiuk <jessica.janiuk@gmail.com>
2016-05-08 19:24:14 -0500

    added deploy and sass folders, updated grunt tasks

--- Gruntfile.js
+++ Gruntfile.js
@@ -9,14 +9,39 @@ module.exports = function(grunt) {
         }
       }
     },
+    sass: {                              // Task
+      dist: {                            // Target
+        options: {                       // Target options
+          style: 'expanded'
+        },
+        files: {                         // Dictionary of files
+          'dist/css/site.css': 'sass/site.scss',       // 'destination': 'source'
+        }
+      }
+    },
     watch: {
-      files: ['<%= jshint.files %>'],
-      tasks: ['jshint']
+      css: {
+        files: '**/*.scss',
+        tasks: ['sass']
+      },
+      jshint: {
+        files: ['<%= jshint.files %>'],
+        tasks: ['jshint']
+      }
+    },
+    copy: {
+      main: {
+        files: [
+          {expand: true, cwd: 'bower_components/Materialize/fonts/', src: ['**'], dest: 'dist/fonts'},
+          {expand: true, cwd: 'bower_components/modernizr/', src: ['modernizr.js'], dest: 'dist/js'},
+        ]
+      }
     }
   });
 
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-copy');
   grunt.loadNpmTasks('grunt-contrib-sass');
 
   grunt.registerTask('default', ['jshint']);
