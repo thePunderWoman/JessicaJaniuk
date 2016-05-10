4de4f9231e596fe91e7ee402c523800b2d78aeaf
Jessica Janiuk <jessica.janiuk@gmail.com>
2016-05-09 20:37:05 -0500

    Progress, added firebase config, got babel configured, started setting up angular

--- Gruntfile.js
+++ Gruntfile.js
@@ -19,31 +19,53 @@ module.exports = function(grunt) {
         }
       }
     },
+    browserify: {
+       dist: {
+          options: {
+             transform: [
+                ["babelify"]
+             ]
+          },
+          files: {
+               "./dist/app/app.js": ["./src/app.js"]
+          }
+       }
+    },
+    copy: {
+      main: {
+        files: [
+          {expand: true, cwd: 'bower_components/Materialize/dist/font/', src: ['**'], dest: 'dist/fonts'},
+          {expand: true, cwd: 'bower_components/Materialize/dist/js/', src: ['**'], dest: 'dist/js'},
+          {expand: true, cwd: 'bower_components/modernizr/', src: ['modernizr.js'], dest: 'dist/js'},
+          {expand: true, cwd: 'bower_components/angular/', src: ['angular.min.js', 'angular.min.js.map'], dest: 'dist/js/angular'},
+          {expand: true, cwd: 'bower_components/angular-ui-router/release', src: ['angular-ui-router.min.js'], dest: 'dist/js/angular'},
+          {expand: true, cwd: 'bower_components/angularfire/dist/', src: ['angularfire.min.js'], dest: 'dist/js/angular'},
+          {expand: true, cwd: 'bower_components/firebase/', src: ['firebase.js'], dest: 'dist/js'},
+          {expand: true, cwd: 'app/', src: ['*.js'], dest: 'dist/app'},
+        ]
+      }
+    },
     watch: {
       css: {
         files: '**/*.scss',
         tasks: ['sass']
       },
-      jshint: {
-        files: ['<%= jshint.files %>'],
-        tasks: ['jshint']
+      scripts: {
+        files: ["src/*.js"],
+        tasks: ["browserify"]
       }
-    },
-    copy: {
-      main: {
-        files: [
-          {expand: true, cwd: 'bower_components/Materialize/fonts/', src: ['**'], dest: 'dist/fonts'},
-          {expand: true, cwd: 'bower_components/modernizr/', src: ['modernizr.js'], dest: 'dist/js'},
-        ]
-      }
-    }
+    },    
   });
 
+  grunt.loadNpmTasks("grunt-browserify");
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-copy');
   grunt.loadNpmTasks('grunt-contrib-sass');
 
   grunt.registerTask('default', ['jshint']);
+  grunt.registerTask('default', ['copy']);
+  grunt.registerTask('default', ['watch']);
+  grunt.registerTask("build", ["browserify"]);
 
 };
