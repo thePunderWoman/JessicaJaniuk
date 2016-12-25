de691440c50ead653082136506bc50280c57d518
Jessica Janiuk <jessica.janiuk@gmail.com>
2016-12-24 22:15:43 -0600

    Added remaining major dependencies, added about page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,18 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { RouterModule, Routes } from '@angular/router';
+import { MaterialModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
 import { NavigationComponent } from './navigation/navigation.component';
 import { HomeComponent } from './home/home.component';
 import { LogoHeaderComponent } from './logo-header/logo-header.component';
+import { AboutComponent } from './about/about.component';
+import 'hammerjs';
 
 const appRoutes: Routes = [
   { path: '', component: HomeComponent },
+  { path: 'about', component: AboutComponent }
 ];
 
 @NgModule({
@@ -18,13 +22,15 @@ const appRoutes: Routes = [
     AppComponent,
     NavigationComponent,
     HomeComponent,
-    LogoHeaderComponent
+    LogoHeaderComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
-    RouterModule.forRoot(appRoutes)
+    RouterModule.forRoot(appRoutes),
+    MaterialModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
