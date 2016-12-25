e8c8082b4c7a2e9bb0cf617c8986a8c8da62776d
Jessica Janiuk <jessica.janiuk@gmail.com>
2016-12-25 15:06:28 -0600

    added title tag to 404 page

--- src/app/notfound/notfound.component.ts
+++ src/app/notfound/notfound.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Title }     from '@angular/platform-browser';
 
 @Component({
   selector: 'app-notfound',
@@ -7,9 +8,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NotFoundComponent implements OnInit {
 
-  constructor() { }
+  constructor(private titleService: Title) { }
 
   ngOnInit() {
+    this.titleService.setTitle('404! Not Found | Jessica Janiuk');
   }
-
 }
