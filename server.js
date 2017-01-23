24ad30e9ff1c5240f9ad18517fae39aa3b18efaf
Jessica Janiuk <jessica.janiuk@gmail.com>
2017-01-22 23:14:21 -0500

    force ssl

--- server.js
+++ server.js
@@ -5,6 +5,24 @@ const app = express();
 // Gzip
 const compression = require('compression');
 
+// If an incoming request uses
+// a protocol other than HTTPS,
+// redirect that request to the
+// same url but with HTTPS
+const forceSSL = function() {
+  return function (req, res, next) {
+    if (req.headers['x-forwarded-proto'] !== 'https') {
+      return res.redirect(
+       ['https://', req.get('Host'), req.url].join('')
+      );
+    }
+    next();
+  }
+}
+// Instruct the app
+// to use the forceSSL
+// middleware
+app.use(forceSSL());
 app.use(compression());
 
 // Run the app by serving the static files
