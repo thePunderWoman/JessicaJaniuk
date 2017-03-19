bc05e97ad13c2fc477969b7b5744c4bbccf1fa28
Jessica Janiuk <jessica.janiuk@gmail.com>
2017-03-19 16:14:20 -0400

    updated to latest angular-cli version, update all node packages

--- src/typings.d.ts
+++ src/typings.d.ts
@@ -1,3 +1,5 @@
-// Typings reference file, you can add your own global typings here
-// https://www.typescriptlang.org/docs/handbook/writing-declaration-files.html
+/* SystemJS module definition */
+declare var module: {
+  id: string;
+};
 declare var moment: any;
